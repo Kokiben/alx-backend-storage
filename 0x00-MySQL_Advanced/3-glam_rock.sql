5482b19c65c099689e222c7fad71ba3a53136794
Kokiben <bennassarkaoutar@gmail.com>
2024-10-18 00:43:17 +0000

    glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- SQL script to list all Glam rock bands ranked by their longevity
--- Columns: band_name and lifespan (in years until 2022)
-SELECT band_name, 
-       (2022 - formed) AS lifespan 
-FROM metal_bands 
-WHERE style = 'Glam rock' 
+-- Columns: band_name and lifespan (in years until 2022 or split)
+SELECT band_name,
+       (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
