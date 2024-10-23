0346432ad3f94a46390ca22a1e30e4abc191cca7
Kokiben <bennassarkaoutar@gmail.com>
2024-10-22 19:13:12 -0700

    update

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """
-Insert a new document in Python
+Add a new document to a collection using keyword arguments.
 """
 
 
-def insert_school(mongo_collection: Collection, **kwargs) -> str:
+def insert_school(mongo_collection, **kwargs) -> str:
     """
-    Inserts a new document into the MongoDB collection based on kwargs.
-    
-    :param mongo_collection: The pymongo collection object
-    :param kwargs: The key-value pairs for the new document
-    :return: The _id of the inserted document
+    Adds a n document to MongoDB collection based on provided keyword arg.
+
+    :param mongo_collection: The pymongo collection object.
+    :param kwargs: The key-value pairs representing the new document.
+    :return: The _id of the newly inserted document.
     """
-    result = mongo_collection.insert_one(kwargs)
-    return str(result.inserted_id)
+    new_document = mongo_collection.insert_one(kwargs)
+    return str(new_document.inserted_id)
