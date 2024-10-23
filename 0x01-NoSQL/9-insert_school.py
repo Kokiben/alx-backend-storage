0628e2fd8397a99ceb6ece76ee9ffe6290f6fbbc
Kokiben <bennassarkaoutar@gmail.com>
2024-10-22 19:18:55 -0700

    school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,7 +4,7 @@ Add a new document to a collection using keyword arguments.
 """
 
 
-def insert_school(mongo_collection, **kwargs) -> str:
+def insert_school(mongo_collection, **kwargs):
     """
     Adds a n document to MongoDB collection based on provided keyword arg.
 
@@ -12,5 +12,5 @@ def insert_school(mongo_collection, **kwargs) -> str:
     :param kwargs: The key-value pairs representing the new document.
     :return: The _id of the newly inserted document.
     """
-    new_document = mongo_collection.insert_one(kwargs)
-    return str(new_document.inserted_id)
+    n_dcmnt = mongo_collection.insert_one(kwargs)
+    return n_dcmnt.inserted_id
